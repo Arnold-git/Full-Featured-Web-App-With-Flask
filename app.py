22a1eedb808143fb81eeab6c009c00fcc3103f3b
Arnold N.I <arnold.ighiwiyisi@eng.uniben.edu>
2020-11-04 23:05:30 +0100

    db setup

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
+
 app = Flask(__name__)
-
-
 app.config['SECRET_KEY'] = 'c61d0b0167fa2d8681a9f80c56b4bc2a'
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullabel=False)
+
+
+
 
 posts = [
     {
