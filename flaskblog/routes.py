1c89f2735359e3e5997fc2acf38654a11080dc33
Arnold N.I <arnold.ighiwiyisi@eng.uniben.edu>
2020-11-18 17:03:02 +0100

    account route secured

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -68,6 +68,7 @@ def logout():
     return redirect(url_for('home'))
 
 @app.route("/account")
+@login_required
 def account():
      return render_template('account.html', title='Account')
 
