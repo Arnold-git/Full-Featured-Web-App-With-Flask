5b62aba4a5af651d2c0a8d7cd464b186a19262f0
Arnold N.I <arnold.ighiwiyisi@eng.uniben.edu>
2020-11-19 12:55:29 +0100

    profile pic upload feature added

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -57,16 +58,20 @@ class UpdateAccountForm(FlaskForm):
 	email = StringField('Email',
 		validators=[DataRequired(), Email()])
 
+	picture = FileField('Update Profile Picture', 
+		validators=[FileAllowed(['jpg','png'])])
+
 	submit = SubmitField('Update')
 
 
+
 	def validate_username(self, username): 
-		if username.data != curremt_user.username:
+		if username.data != current_user.username:
 			user = User.query.filter_by(username=username.data).first()
 			if user:
 				raise ValidationError('Username already exist,Please choose a different one')
 	def validate_email(self, email):
-		if email.data != curremt_user.email:
+		if email.data != current_user.email:
 
 			user = User.query.filter_by(username=email.data).first()
 			if user:
