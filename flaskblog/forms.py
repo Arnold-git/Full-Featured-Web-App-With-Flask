eecc24a0634cc7d9aacf50dcbb7570e78d9b8443
Arnold N.I <arnold.ighiwiyisi@eng.uniben.edu>
2020-11-19 11:56:23 +0100

    account form setup

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -45,3 +46,28 @@ class LoginForm(FlaskForm):
 	remember = BooleanField('Remember Me')
 
 	submit = SubmitField('Login')		
+
+
+class UpdateAccountForm(FlaskForm):
+
+	"""docstring for RegistrationForm"""
+
+	username = StringField('Username', validators=[DataRequired(),
+		Length(min=2, max=20)])
+	email = StringField('Email',
+		validators=[DataRequired(), Email()])
+
+	submit = SubmitField('Update')
+
+
+	def validate_username(self, username): 
+		if username.data != curremt_user.username:
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError('Username already exist,Please choose a different one')
+	def validate_email(self, email):
+		if email.data != curremt_user.email:
+
+			user = User.query.filter_by(username=email.data).first()
+			if user:
+				raise ValidationError('Email already exist,Please choose a different one')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -71,6 +71,8 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+    form = UpdateAccountForm()
     image_file = url_for('static', filename='profile_pic/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file,
+        form=form)
 
