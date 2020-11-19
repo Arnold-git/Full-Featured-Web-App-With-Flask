8ef31e3f99409b07d5a6ea3184fbf5dd11b7dc6e
Arnold N.I <arnold.ighiwiyisi@eng.uniben.edu>
2020-11-19 16:29:21 +0100

    bug fixe

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForn
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -113,7 +113,7 @@ def account():
 @app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
-    form = PostForn()
+    form = PostForm()
     if form.validate_on_submit():
         flash("Your post has been created", 'suceess')
         return redirect(url_for('home'))
