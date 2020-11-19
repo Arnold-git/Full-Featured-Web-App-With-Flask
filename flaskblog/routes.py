d3e0bb437cb7e5441116a1b379764ec67e39447f
Arnold N.I <arnold.ighiwiyisi@eng.uniben.edu>
2020-11-19 11:41:13 +0100

    profile pic feature added

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -71,5 +71,6 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-     return render_template('account.html', title='Account')
+    image_file = url_for('static', filename='profile_pic/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file)
 
