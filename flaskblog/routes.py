606257f9ec5b9558a443ea2188833ab3d3161771
Arnold N.I <arnold.ighiwiyisi@eng.uniben.edu>
2020-11-20 17:50:22 +0100

    author's name in home page enabled

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -12,6 +12,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
