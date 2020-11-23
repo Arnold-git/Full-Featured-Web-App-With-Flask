ae2360f1c623a3f1229893e0a06f2035946dde21
Arnold N.I <arnold.ighiwiyisi@eng.uniben.edu>
2020-11-23 21:47:26 +0100

    pagination complete

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -153,7 +153,7 @@ def delete_post(post_id):
 
 
 
-@app.route("/user/<str:username>")
+@app.route("/user/<string:username>")
 def user_posts(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
